b152af54b3ff67f711f5d902151ece312ffc5b0e
saurabh gaikwad <saurabhgaikwad1567@gmail.com>
2025-10-19 18:09:57 +0530

    Initial commit

---
+++ Day1/src/FibonacciSeries.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=0;
+        int b=1;
+        System.out.println("enter limit to print series");
+        int limit = sc.nextInt();
+        for(int i=0;i<=limit;i++){
+            System.out.print(a+", ");
+            int c = a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
